b5efeedf82d5d8b5f054d924d022e7235c007eda
ramiel83 <ramiel83@gmail.com>
2023-07-26 18:01:08 +0300

    wip

--- client/src/UI/Notification.js
+++ client/src/UI/Notification.js
@@ -1,30 +1,28 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export const showAlert = (message, type = 'success') => (dispatch) => {
+export const showAlert =
+  (message, type = "success") =>
+  (dispatch) => {
     switch (type) {
-      case 'success':
+      case "success":
         toast.success(message, {
           position: toast.POSITION.TOP_CENTER,
-          // Add any other options you want for success alerts
         });
         break;
-      case 'error':
+      case "error":
         toast.error(message, {
           position: toast.POSITION.TOP_CENTER,
-          // Add any other options you want for error alerts
         });
         break;
-      case 'warning':
+      case "warning":
         toast.warn(message, {
           position: toast.POSITION.TOP_CENTER,
-          // Add any other options you want for warning alerts
         });
         break;
       default:
         toast.info(message, {
           position: toast.POSITION.TOP_CENTER,
-          // Add any other options you want for info alerts
         });
         break;
     }
